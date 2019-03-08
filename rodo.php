723bafb5b18d9f82cd9c9bc124ab260e153926a8
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2019-03-08 14:47:29 +0100

    Fix mobile device viewport

--- rodo.php
+++ rodo.php
@@ -2377,6 +2377,7 @@ if (isset($_GET['article'])) {
 <html lang="pl">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
   <style>
     body {
       color: #444444;
